1e89732f74addccdda84976b82745911ce678603
subenakhatun <subenakhtun@gmail.com>
2019-04-09 00:47:21 +0600

    Add files via upload

---
+++ online1.py
@@ -0,0 +1,29 @@
+# t = int(input("Enter a integer number: "))
+# if t < 15:
+#     a = input('Enter a value: ')
+#     b = input('Enter a value: ')
+#
+#     if a < b:
+#         print('<')
+#     a = input('Enter a value: ')
+#     b = input('Enter a value: ')
+#     if a > b:
+#         print('<')
+#
+#     a = input('Enter a value: ')
+#     b = input('Enter a value: ')
+#     if a == b:
+#         print('=')
+# else:
+#     print('please input correct value'
+
+t = int(input("Enter a integer number: "))
+if t < 15:
+    a = input('Enter a value: ')
+    b = input('Enter a value: ')
+    if a < b:
+        print('<')
+    elif a > b:
+        print('<')
+    elif a == b:
+        print('=')
